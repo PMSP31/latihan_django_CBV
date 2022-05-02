7c1e4b41b261cec8b1129e8ae50b0c37bf4e3fb4
PMSP31 <pmsp3108@gmail.com>
2022-05-02 21:47:22 +0700

    Login & SignUp

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from .views import BlogView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BlogView.as_view(), name='index'),
+    path('account/',include('django.contrib.auth.urls')),
+    path('account/', include('account.urls', namespace = 'account')),
     path('article/', include('article.urls', namespace = 'article'))
 ]
